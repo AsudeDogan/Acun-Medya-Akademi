73a6b4f104d6cd1e267783c9c51ee7e0daf8bc61
AsudeDogan <168633588+AsudeDogan@users.noreply.github.com>
2025-03-07 23:55:16 +0300

    Add files via upload

---
+++ NET& REACT ÖDEV 3/Employee.cs
@@ -0,0 +1,20 @@
+﻿public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Salary { get; set; }
+    public string Department { get; set; }
+
+    public Employee(int id, string name, double salary, string department)
+    {
+        Id = id;
+        Name = name;
+        Salary = salary;
+        Department = department;
+    }
+
+    public virtual double CalculateBonus()
+    {
+        return 0; 
+    }
+}
---
+++ NET& REACT ÖDEV 3/Manager.cs
@@ -0,0 +1,17 @@
+namespace Question1;
+
+public class Manager : Employee
+{
+    public int TeamSize { get; set; }
+
+    public Manager(int id, string name, double salary, string department, int teamSize)
+        : base(id, name, salary, department)
+    {
+        TeamSize = teamSize;
+    }
+
+    public override double CalculateBonus()
+    {
+        return Salary * 0.2; //  %20 prim 
+    }
+}
