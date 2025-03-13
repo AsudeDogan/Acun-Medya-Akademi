8195b3e0a886650d1ab0de422f508ea077c31134
AsudeDogan <168633588+AsudeDogan@users.noreply.github.com>
2025-03-13 14:45:22 +0300

    Add files via upload

---
+++ Ocak1.HaftaNetÖdev/Soru1veSoru2.cs
@@ -0,0 +1,12 @@
+//Soru1: C# programlama dili nedir? Hangi alanlarda kullanılır?
+
+//nesne yönelimli, güçlü ve modern bir programlama dilidir.
+//Web, masaüstü, mobil uygulamalar, oyun geliştirme, yapay zeka ve kurumsal yazılımlar gibi birçok alanda kullanılır.
+
+//Soru2: Bir programın çalışması için temel bileşenler nelerdir? (Derleyici, RAM, CPU vb.)
+
+//Derleyici (Compiler) → Kaynak kodunu makine diline çevirir.
+// CPU (İşlemci) → Programın komutlarını işler ve yürütür.
+// RAM (Bellek) → Programın çalışma sırasında verileri sakladığı geçici bellek.
+// Depolama (SSD/HDD) → Programın dosyalarının ve verilerinin kalıcı olarak saklandığı yer.
+// İşletim Sistemi (OS) → Programın donanım ile iletişimini sağlayan ortam (Windows, Linux vb.).
---
+++ Ocak1.HaftaNetÖdev/Soru3.cs
@@ -0,0 +1,6 @@
+//Soru3: Kullanıcıdan adını alıp ekrana yazdıran basit bir C# programı yazın.
+
+Console.WriteLine("Adınızı Giriniz:");
+string name = Console.ReadLine();
+
+Console.WriteLine(name);
